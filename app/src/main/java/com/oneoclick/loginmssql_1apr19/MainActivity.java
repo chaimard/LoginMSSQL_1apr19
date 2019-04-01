4adad7159f090c56316bb2fe3cc761e909450398
chaimard <chaimard@email.krirk.ac.th>
2019-04-01 18:08:33 +0700

    completely work 100%

--- app/src/main/java/com/oneoclick/loginmssql_1apr19/MainActivity.java
+++ app/src/main/java/com/oneoclick/loginmssql_1apr19/MainActivity.java
@@ -1,11 +1,17 @@
 package com.oneoclick.loginmssql_1apr19;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,11 +34,72 @@ public class MainActivity extends AppCompatActivity {
         pbbar.setVisibility(View.GONE);
 
 
-        
-
-
-
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //edtuserid.setText("Test");
+                //Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT);
+                DoLogin doLogin = new DoLogin();
+                doLogin.execute("");
+            }
+        });
 
     }//onCreate
+
+    public class DoLogin extends AsyncTask<String, String, String> {
+        String message = "";
+        Boolean isSuccess = false;
+
+        String userid = edtuserid.getText().toString();
+        String password = edtpass.getText().toString();
+
+        @Override
+        protected void onPreExecute() {
+            pbbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String r) {
+            pbbar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this,r,Toast.LENGTH_SHORT).show();
+
+            if (isSuccess) {
+                Toast.makeText(MainActivity.this,r,Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if (userid.trim().equals("")||password.trim().equals("")) {
+                message = "Please enter User ID and Password";
+            } else {
+                try {
+                    Connection con = connectionClass.CONN();
+                    if (con == null) {
+                        message = " Error in connection with SQL Server";
+                    } else {
+                        String query = "select * from User1 where userid ='" + userid + "' and userpassword='" + password + "'";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+
+                        if (rs.next()) {
+                            String username = rs.getString("UserFname")+" "+rs.getString("UserLname");
+                            message = "Login Sucessfull...Hello Khun: " + userid + ":"+username;
+                            isSuccess = true;
+                        } else {
+                            message = "Invalid Credentials " ;
+                            isSuccess = false;
+                        }
+                    }
+
+                } catch (Exception ex) {
+                    isSuccess = false;
+                    message = " Exceptions";
+                }
+            }
+            return message;
+        }//doInBackground
+
+    }//DoLogin
 
 }//main
